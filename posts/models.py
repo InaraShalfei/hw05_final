1bc080aa3df14f237d4dfb52c57e5cbc51203170
Инара <inarashugaikina@gmail.com>
2021-02-15 18:38:14 +0600

    Create Follow model

--- posts/models.py
+++ posts/models.py
@@ -57,3 +57,11 @@ class Comment(models.Model):
     created = models.DateTimeField("Дата комментария",
                                    auto_now_add=True)
 
+
+class Follow(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE,
+                             related_name="follower",
+                             verbose_name="Подписчик")
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="following",
+                               verbose_name="Автор")
